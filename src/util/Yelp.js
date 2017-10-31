4a2d0ea8f142a3819e4713c5b5ba89d7421b4489
nasseralrayes <31788087+nasseralrayes@users.noreply.github.com>
2017-10-31 17:17:30 +0300

    Add files via upload

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -6,9 +6,10 @@ const Yelp = {
   getAccessToken () {
     if (accessToken) {
       return new Promise(resolve => resolve(accessToken));
-    } return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type
-      =client_credentials&client_id=${clientId}&client_secret=${secret}`, 
-      {method: 'POST'}).then(response => {
+    } return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, 
+      {
+        method: 'POST'
+      }).then(response => {
         return response.json();
       }).then(jsonResponse => {
         accessToken = jsonResponse.access_token;
@@ -17,8 +18,9 @@ const Yelp = {
 
   search(term, location, sortBy) {
     return Yelp.getAccessToken().then(() => {
-      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?
-        term=${term}&location=${location}&sort_by=${sortBy}`, {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com
+        /v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+      {
           headers: {Authorization: `Bearer ${accessToken}`}
         });
     }).then(response => {
